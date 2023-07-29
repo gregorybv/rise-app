f32c0b643e58e2a9007045b4376ebbc574f9caf0
Gregory <gregory_berezin@outlook.com>
2023-07-29 16:25:46 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import Navbar from "./components/navar/Navbar.tsx";
+import Power from "./components/power/Power.tsx";
+import Speed from "./components/speed/Speed.tsx";
+import Handling from "./components/handling/Handling.tsx";
+import Options from "./components/options/Options.tsx";
+import Contact from "./components/contact/Contact.tsx";
+
+function App() {
+
+
+    return (
+        <>
+            <Navbar/>
+            <Power/>
+            <Speed/>
+            <Handling/>
+            <Options/>
+            <Contact/>
+        </>
+    )
+}
+
+export default App
